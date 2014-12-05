39002f7ce3213646432df8b6045895fc21c19fb5
Sebastien Pierre <sebastien.pierre@gmail.com>
2014-12-05 12:27:05 -0500

    WIP - good progress on the core parser

--- pythoniccss.py
+++ pythoniccss.py
@@ -13,6 +13,7 @@ def doIndent(context):
 	v = context.getVariables().getParent ()
 	i = v.get("requiredIndent") or 0
 	v.set("requiredIndent", i + 1)
+	return True
 
 def doCheckIndent(context):
 	v          = context.getVariables()
@@ -21,11 +22,11 @@ def doCheckIndent(context):
 	req_indent = v.get("requiredIndent") or 0
 	return tab_indent == req_indent
 
-
 def doDedent(context):
 	v = context.getVariables().getParent ()
 	i = v.get("requiredIndent") or 0
 	v.set("requiredIndent", i - 1)
+	return True
 
 # -----------------------------------------------------------------------------
 #
@@ -40,20 +41,31 @@ def grammar(g=Grammar("PythonicCSS")):
 	g.token   ("EMPTY_LINES",      "([ \t]*\n)+")
 	g.token   ("INDENT",           "\t+")
 	g.token   ("COMMENT",          "[ \t]*\#[^\n]*")
+	g.token   ("EQUAL",             "=")
 	g.token   ("EOL",              "[ ]*\n(\s*\n)*")
 	g.token   ("NUMBER",           "-?(0x)?[0-9]+(\.[0-9]+)?")
-
+	g.token   ("SUFFIX",           ":[a-z][a-z0-9\-]*")
+	g.token   ("SELECTION_OPERATOR", "\>")
+	g.word    ("INCLUDE",             "%include")
 	g.word    ("COLON",            ":")
+	g.word    ("SELF",             "&")
+	g.word    ("COMMA",            ",")
+
+	g.token   ("PATH",             "\"[^\"]+\"|'[^']'|[^\s\n]+")
+	g.token   ("PERCENTAGE",       "\d+(\.\d+)%")
 
 	g.token   ("NODE",             "[a-zA-Z][a-zA-Z0-9\-]*")
 	g.token   ("NODE_CLASS",       "\.[a-zA-Z][a-zA-Z0-9\-]*")
 	g.token   ("NODE_ID",          "#[a-zA-Z][a-zA-Z0-9\-]*")
-	g.token   ("UNIT",             "em|pt|px|rem|\%")
-	g.token   ("REFERENCE",        "\$[\w\d_]+")
+	# SEE: http://www.w3schools.com/cssref/css_units.asp
+	g.token   ("UNIT",             "em|ex|px|cm|mm|in|pt|pc|ch|rem|vh|vmin|vmax|\%")
+	g.token   ("VARIABLE_NAME",    "[\w_][\w\d_]*")
+	g.token   ("REFERENCE",        "\$[\w_][\w\d_]*")
+	g.token   ("COLOR_NAME",       "[a-z][a-z0-9\-]*")
 	g.token   ("COLOR_HEX",        "\#\[A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9]([A-Fa-f0-9][A-Fa-f0-9])?")
 	g.token   ("COLOR_RGB",        "rgba?\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*(,\s*\d+\s*)?\)")
-
-	g.token   ("CSS_PROPERTY",     "[a-z][a-z0-9\-]*")
+	g.token   ("CSS_PROPERTY",    "[a-z][a-z0-9\-]*")
+	g.token   ("SPECIAL_NAME",     "@[A-Za-z][A-Za-z0-9\_]*")
 
 	# =========================================================================
 	# INDENTATION
@@ -63,31 +75,51 @@ def grammar(g=Grammar("PythonicCSS")):
 	g.procedure ("Dedent",           doDedent)
 	g.rule      ("CheckIndent",      s.TABS.bindAs("tabs"), g.acondition(doCheckIndent)).disableMemoize ()
 
-	g.rule      ("Comment",          s.COMMENT.oneOrMore())
 
-	g.rule      ("Selector",         g.agroup(s.NODE, s.NODE_ID, s.NODE_CLASS))
+	# TODO: support nth(10)
+	g.rule      ("Selector",         g.agroup(s.SELF, s.NODE, s.NODE_ID, s.NODE_CLASS), s.SUFFIX.optional())
+	g.rule      ("SelectorNarrower", s.SELECTION_OPERATOR.optional(), s.Selector)
+	g.rule      ("Selection",        g.agroup(s.Selector), s.SelectorNarrower.zeroOrMore ())
 
 	# =========================================================================
 	# VALUES & EXPRESSIONS
 	# =========================================================================
 
 	g.rule      ("Number",           s.NUMBER, s.UNIT.optional())
-	g.group     ("Value",            s.Number, s.REFERENCE)
-	g.rule      ("Property",         s.CheckIndent, s.CSS_PROPERTY, s.COLON, s.Value)
+	g.group     ("Value",            s.Number, s.REFERENCE, s.COLOR_NAME)
+	g.group     ("Expression",       s.Value)
+
+	g.rule      ("Parameters",       s.VARIABLE_NAME, g.arule(s.COMMA, s.VARIABLE_NAME).zeroOrMore())
+
+	# =========================================================================
+	# LINES (BODY)
+	# =========================================================================
+
+	g.rule      ("Comment",          s.COMMENT.oneOrMore(), s.EOL.optional())
+	g.rule      ("Declaration",      s.CheckIndent, s.VARIABLE_NAME, s.EQUAL, s.Expression, s.EOL)
+	g.rule      ("Assignment",       s.CheckIndent, s.CSS_PROPERTY, s.COLON, s.Expression, s.EOL)
+	g.rule      ("Include",          s.CheckIndent, s.INCLUDE, s.PATH, s.EOL)
 
 	# =========================================================================
 	# BLOCK STRUCTURE
 	# =========================================================================
 
-	g.group     ("Rule",             s.Property, s.Comment)
-	g.rule      ("Selection",        g.agroup(s.Selector), s.COLON, s.Indent, s.Rule, s.Dedent)
+	g.rule    ("Rule")
+	g.group   ("RuleLine",        s.Rule, s.Comment, s.Assignment)
+	g.rule    ("RuleBody",        s.Indent, s.RuleLine.zeroOrMore(), s.Dedent)
+	g.rule    ("RuleSelection",   s.CheckIndent, g.agroup(s.Selection, s.PERCENTAGE), s.COLON)
+	s.Rule.set(s.RuleSelection, s.EOL, s.RuleBody)
+
+	g.rule    ("SpecialDeclaration",   s.CheckIndent, s.SPECIAL_NAME, s.VARIABLE_NAME, s.Parameters.optional(), s.COLON)
+	g.rule    ("SpecialBlock",         s.SpecialDeclaration, s.EOL, s.RuleBody)
+
 
 	# =========================================================================
 	# AXIOM
 	# =========================================================================
 
-	g.group     ("Source",  g.agroup(s.Comment, s.Rule).zeroOrMore())
-	g.ignore    (s.SPACE, s.COMMENT)
+	g.group     ("Source",  g.agroup(s.Comment, s.SpecialBlock, s.Rule, s.Declaration, s.Include).zeroOrMore())
+	g.ignore    (s.SPACE, s.COMMENT, s.EOL)
 	g.axiom     = s.Source
 	return g
 
@@ -114,23 +146,26 @@ def compile(path, treebuilderClass, grammar=None):
 	return builder.build(parse(path, grammar), grammar)
 
 if __name__ == "__main__":
-	import sys
+	import sys, os
 	args = sys.argv[1:]
+	getGrammar().log.verbose = True
+	getGrammar().log.level   = 100
+	getGrammar().log.enabled = True
 	for path in args:
 		print parse(path)
 
-Todo, what should happen:
-"""
-Parser failed with rest:  'div:\n\twidth: 100%\n'
- at line (0, 0)
-    div:
- *
-	width: 100%
-
-Parse tree is:
-	Source(a=xx,b=xxx,c=xxx)
-		XXX..
-			XXX.
-None
-"""
+# Todo, what should happen:
+# """
+# Parser failed with rest:  'div:\n\twidth: 100%\n'
+#  at line (0, 0)
+#     div:
+#  *
+# 	width: 100%
+#
+# Parse tree is:
+# 	Source(a=xx,b=xxx,c=xxx)
+# 		XXX..
+# 			XXX.
+# None
+# """
 # EOF
