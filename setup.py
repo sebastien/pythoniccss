3761da3466bbb79574b9bf779ff1b63b87c8db6a
Sebastien Pierre <sebastien.pierre@gmail.com>
2014-12-24 10:28:05 -0500

    Added setup.py with RST transformation

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 # Encoding: utf-8
 # See: <http://docs.python.org/distutils/introduction.html>
+import os
 try:
 	from setuptools import setup
 except ImportError:
 	from distutils.core import setup
 
-VERSION = eval(filter(lambda _:_.startswith("VERSION"),
- file("src/pythoniccss.py").readlines())[0].split("=")[1])
+VERSION = eval(filter(lambda _:_.startswith("VERSION"), file("src/pythoniccss.py").readlines())[0].split("=")[1])
+# If pandoc is installed, we translate the documentation to RST
+LONG_DESCRIPTION  = None
+if os.popen("which pandoc").read():
+	LONG_DESCRIPTION = os.popen("pandoc -f markdown -t rst README.md").read()
 
 setup(
 	name             = "pythoniccss",
 	version          = VERSION,
 	description      = "A Pythonic CSS pre-processor, designed as a replacement/upgrade to CleverCSS",
+	long_description = LONG_DESCRIPTION,
 	author           = "Sébastien Pierre",
 	author_email     = "sebastien.pierre@gmail.com",
 	url              = "http://github.com/sebastien/pythoniccss",
